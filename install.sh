966028a0b1d3d39c6d24e40bdd2fd520c87482f6
Yekinni Azeez <yekinniazeez.cloud@gmail.com>
2023-11-15 18:38:28 +0100

    Added the installation script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set the installation directory
+INSTALL_DIR="/usr/local/bin"
+
+# Copy the main script to the installation directory
+cp clouduploader.sh $INSTALL_DIR
+
+# Provide execute permissions
+chmod +x $INSTALL_DIR/clouduploader.sh
+
+echo "CloudUploader is now installed."
+echo "You can use it by running 'clouduploader.sh' from any directory."
+
+
